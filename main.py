0cfb8c1d8a85dab0a6869aa550941af3e52e46b4
after <after@DESKTOP-UT5INKU>
2025-06-01 22:25:16 +0300

    обновление main.py

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
-from fastapi import FastAPI
+п»їfrom fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
-from app.api.endpoints import documents, analysis, readability
+from app.api import documents, analysis, readabilityAPI
+from app.database.database import Base, engine
 from app.core.config import settings
 
 '''
-Необходимо тут прописать код, который создает создает все таблицы в базе данных
+РќРµРѕР±С…РѕРґРёРјРѕ С‚СѓС‚ РїСЂРѕРїРёСЃР°С‚СЊ РєРѕРґ, РєРѕС‚РѕСЂС‹Р№ СЃРѕР·РґР°РµС‚ СЃРѕР·РґР°РµС‚ РІСЃРµ С‚Р°Р±Р»РёС†С‹ РІ Р±Р°Р·Рµ РґР°РЅРЅС‹С…
 '''
 
-# создание экземпляра FastAPI
 app = FastAPI(
     title=settings.APP_NAME,
-    description="API для анализа текстовых документов",
+    description="API РґР»СЏ Р°РЅР°Р»РёР·Р° С‚РµРєСЃС‚РѕРІС‹С… РґРѕРєСѓРјРµРЅС‚РѕРІ",
     version="1.0.0",
     lifespan=lifespan
 )
 
-# настройка CORS (спец. механизм, который использует HTTP-заголовки и позволяет веб-страницам получать доступ к объектам)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.ALLOW_ORIGINS,
@@ -25,15 +24,14 @@ app.add_middleware(
     allow_headers=settings.ALLOW_HEADERS,
 )
 
-# добавление маршрутов API
 app.include_router(documents.router, prefix=f"{settings.API_PREFIX}/documents", tags=["documents"])
 app.include_router(analysis.router, prefix=f"{settings.API_PREFIX}/analysis", tags=["analysis"])
-app.include_router(readability.router, prefix=f"{settings.API_PREFIX}/readability", tags=["readability"])
+app.include_router(readabilityAPI.router, prefix=f"{settings.API_PREFIX}/readability", tags=["readability"])
 
 @app.get("/", tags=["root"])
 async def root():
     return {
-        "message": "Добро пожаловать в API Текстового Оценщика",
+        "message": "Р”РѕР±СЂРѕ РїРѕР¶Р°Р»РѕРІР°С‚СЊ РІ API РўРµРєСЃС‚РѕРІРѕРіРѕ РћС†РµРЅС‰РёРєР°",
         "version": "1.0.0",
         "docs_url": "/docs"
     }
