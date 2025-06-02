26a2b76ed38d951a5c85ecbd2bab286c6ebeacd2
Mn_Zer0 <zevsoptifinehd@mail.ru>
2025-06-02 05:03:59 +0300

    feat: schemas

---
+++ app/database/schemas.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+from typing import List
+from datetime import datetime
+
+
+class DocumentCreate(BaseModel):
+    title: str
+    content: str
+    word_count: int
+    sentence_count: int
+    readability_score: float
+    keywords: List[str]
+
+class Document(DocumentCreate):
+    id: int
+    created_at: datetime
