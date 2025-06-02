9a003c8ba7a8dc6e73afdc6b22402ebcd962e5ee
Mn_Zer0 <35655185+Mn-Zer0@users.noreply.github.com>
2025-06-02 15:33:43 +0300

    feat: get_document, get_documents

--- app/database/operations_CRUD.py
+++ app/database/operations_CRUD.py
@@ -5,7 +5,7 @@ def create_document(db: sqlite3.Connection, document: DocumentCreate):
     cursor = db.cursor()
     cursor.execute(
         """
-        INSERT INTO documents 
+        INSERT INTO documents
         (title, content, word_count, sentence_count, readability_score, keywords)
         VALUES (?, ?, ?, ?, ?, ?)
         """,
@@ -19,5 +19,50 @@ def create_document(db: sqlite3.Connection, document: DocumentCreate):
     )
     db.commit()
 
-def get_document():
-    pass
+def get_document(db: sqlite3.Connection, document_id: int):
+    cursor = db.cursor()
+    cursor.execute(
+        """
+        SELECT * from documents WHERE id = ?
+        """, (document_id,)
+    )
+    result = cursor.fetchone()
+    print(result)
+
+    document_data = {
+        "id": result[0],
+        "title": result[1],
+        "content": result[2],
+        "word_count": result[3],
+        "sentence_count": result[4],
+        "readability_score": result[5],
+        "keywords": result[6]
+    }
+    return Document(**document_data)
+
+def get_documents(skip: int, limit: int, db: sqlite3.Connection):
+    cursor = db.cursor()
+    cursor.execute(
+        """
+        SELECT id, title, content, word_count, 
+               sentence_count, readability_score, keywords
+        FROM documents
+        ORDER BY id ASC
+        LIMIT ? OFFSET ?
+        """,
+        (limit, skip)
+    )
+    results = cursor.fetchall()
+    documents = []
+    for row in results:
+        document = Document(
+            id=row[0],
+            title=row[1],
+            content=row[2],
+            word_count=row[3],
+            sentence_count=row[4],
+            readability_score=row[5],
+            keywords=row[6]
+        )
+        documents.append(document)
+    return documents
